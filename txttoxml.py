47d9cee825c92ef25a07c347770f1604e39e045c
Emesh <emese.am@gmail.com>
2017-08-07 15:24:22 +0200

    commit próba

--- txttoxml.py
+++ txttoxml.py
@@ -3,7 +3,7 @@
 #
 #  txttoxml.py
 #  
-#  Copyright 2017 Emese <emese@Elitke>
+#  Copyright 2017 Emese Szabó <emese@Elitke>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
